9ae62ffe6c0bf15bc9111fd0d5b59f0dea2c86d7
mroyski <mroyski@gmail.com>
2019-10-29 16:14:23 -0400

    drop down in index page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const fs = require('fs');
+const flist = require('./family.json');
 
 var app = express();
 
@@ -15,12 +16,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 
-let familyData = fs.readFileSync('family.json');
-let familyMembers = JSON.parse(familyData);
-console.log(familyMembers);
-
-app.get('/', function(req, res, next) {
-  res.render('index');
+app.get('/', function(req, res) {
+  console.log(flist);
+  res.render('index', { flist: flist });
 });
 
 app.get('/aboutme', function(req, res) {
@@ -36,11 +34,7 @@ app.get('/contact', function(req, res) {
 });
 
 app.get('/family', function(req, res) {
-  res.render('family', { familyMember: JSON.stringify(familyMember) });
-});
-
-app.get('/family/:firstname', function(req, res) {
-  res.render('person', { Id: req.params.id, Qstr: req.query.qstr });
+  res.render('family');
 });
 
 app.post('/submit-form', (req, res) => {
