ada89c3695067ce4716cc323a2bf6b370e2a892c
mroyski <mroyski@gmail.com>
2019-11-01 16:36:46 -0400

    added comment on database used in code

--- Dump.sql
+++ Dump.sql
@@ -19,6 +19,7 @@
 -- Table structure for table `family`
 --
 
+-- USING DATABASE addressbook
 DROP TABLE IF EXISTS `family`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
